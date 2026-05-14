3c7737794f497a11d24856831bd2f019bc11eeb3
sid <siddharthapandey65093@gmail.com>
2026-05-14 22:13:28 +0530

    initial commit

---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2026 Online Library Management System |<a href="https://github.com/kumarpandule2000" target="_blank"  > Designed by : SIDDHARTH PANDEY 6669</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
